6a5fe264da410f278a19f41a838e781ee4524cd5
Atef Adel <atefadel1996123@gmail.com>
2026-02-04 11:51:47 -0800

    Update ddl_table.sql

--- tb-sync-no-merge/ddl_table.sql
+++ tb-sync-no-merge/ddl_table.sql
@@ -1,1 +1,14 @@
+IF OBJECT_ID('gold.table_1','U') IS NOT NULL
+     DROP TABLE gold.table_1
+CREATE TABLE gold.table_1(
+id INT PRIMARY KEY NOT NULL,
+name nvarchar(20) NOT NULL
+)
+
+IF OBJECT_ID('silver.table_1','U') IS NOT NULL
+     DROP TABLE silver.table_1
+CREATE TABLE silver.table_1(
+id INT PRIMARY KEY NOT NULL ,
+name nvarchar(20) NOT NULL,
+)
 
